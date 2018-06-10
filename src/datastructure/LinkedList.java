5849d4d716127d5db859b5e54d82dcab1c294219
czheo <czheo1987@gmail.com>
2018-06-09 21:20:17 -0700

    add addFirst() and removeFirst() to LinkedList

--- src/datastructure/LinkedList.java
+++ src/datastructure/LinkedList.java
@@ -1,4 +1,12 @@
 package datastructure;
 
 public class LinkedList {
+    // addFirst: append an element to the beginning of the list
+    public void addFirst(int i) {
+
+    }
+    // removeFirst: retrieve and remove the head of the list
+    public int removeFirst() {
+
+    }
 }
