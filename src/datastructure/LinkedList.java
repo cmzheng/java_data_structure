6a9438f923250ae062454133774d8d886f15bb3c
czheo <czheo1987@gmail.com>
2018-06-09 21:31:28 -0700

    add Node class to LinkedList

--- src/datastructure/LinkedList.java
+++ src/datastructure/LinkedList.java
@@ -9,4 +9,30 @@ public class LinkedList {
     public int removeFirst() {
 
     }
+
+    public static class Node {
+        private int value;
+        private Node next;
+
+        public Node(int i) {
+            this.value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+    }
 }
